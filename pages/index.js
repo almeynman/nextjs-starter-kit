25d385ce97fb7348314484df188e088cc485b798
Almas Akchabayev <almas.akchabayev@gmail.com>
2017-03-24 10:27:18 +0600

    Wrap IndexPage in Layout component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { Component, PropTypes } from 'react';
 import 'isomorphic-fetch';
+import Layout from '../components/Layout'
 
 export default class IndexPage extends Component {
   static propTypes = {
@@ -18,61 +19,63 @@ export default class IndexPage extends Component {
 
   render() {
     return (
-      <div className="root">
-        <div className="container">
-          <h1>React.js News</h1>
-          {this.props.news.map(item => (
-            <article key={item.link} className="newsItem">
-              <h1 className="newsTitle"><a href={item.link}>{item.title}</a></h1>
-              <div
-                className="newsDesc"
-                // eslint-disable-next-line react/no-danger
-                dangerouslySetInnerHTML={{ __html: item.content }}
-              />
-            </article>
-          ))}
-        </div>
-        <style jsx>{`
-          .root {
-            padding-left: 20px;
-            padding-right: 20px;
-          }
+      <Layout>
+        <div className="wrapper">
+          <div className="container">
+            <h1>React.js News</h1>
+            {this.props.news.map(item => (
+              <article key={item.link} className="newsItem">
+                <h1 className="newsTitle"><a href={item.link}>{item.title}</a></h1>
+                <div
+                  className="newsDesc"
+                  // eslint-disable-next-line react/no-danger
+                  dangerouslySetInnerHTML={{ __html: item.content }}
+                />
+              </article>
+            ))}
+          </div>
+          <style jsx>{`
+            .wrapper {
+              padding-left: 20px;
+              padding-right: 20px;
+            }
           
-          .container {
-            margin: 0 auto;
-            padding: 0 0 40px;
-            max-width: var(--max-content-width);
-          }
-
-          .newsItem {
-            margin: 0 0 2rem;
-          }
-
-          .newsTitle {
-            font-size: 1.5rem;
-          }
-
-          .newsDesc {
-            h1,
-            h2,
-            h3,
-            h4,
-            h5,
-            h6 {
-              font-size: 1.125rem;
+            .container {
+              margin: 0 auto;
+              padding: 0 0 40px;
+              max-width: var(--max-content-width);
             }
 
-            pre {
-              white-space: pre-wrap;
-              font-size: 0.875rem;
+            .newsItem {
+              margin: 0 0 2rem;
             }
 
-            img {
-              max-width: 100%;
+            .newsTitle {
+              font-size: 1.5rem;
             }
-          }
-        `}</style>
-      </div>
+
+            .newsDesc {
+              h1,
+              h2,
+              h3,
+              h4,
+              h5,
+              h6 {
+                font-size: 1.125rem;
+              }
+
+              pre {
+                white-space: pre-wrap;
+                font-size: 0.875rem;
+              }
+
+              img {
+                max-width: 100%;
+              }
+            }
+          `}</style>
+        </div>
+      </Layout>
     );
   }
 }
