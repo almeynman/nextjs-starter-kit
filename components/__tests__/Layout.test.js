c2f79ed370c7416a5bf02eb2f9fa8812f859956e
Almas Akchabayev <almas.akchabayev@gmail.com>
2017-03-24 10:38:29 +0600

    Add test for Layout component

---
+++ components/__tests__/Layout.test.js
@@ -0,0 +1,17 @@
+/* eslint-env mocha */
+
+import { expect } from 'chai';
+import { render } from 'enzyme';
+import Layout from '../Layout';
+
+describe('Layout', () => {
+  it('renders children correctly', () => {
+    const wrapper = render(
+      <Layout>
+        <div className="child" />
+      </Layout>,
+    );
+
+    expect(wrapper.find('div.child').length).to.equal(1);
+  });
+});
