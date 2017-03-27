69533582c37623f9cbf2fe17e516470425bc6590
Almas Akchabayev <almas.akchabayev@gmail.com>
2017-03-27 17:13:21 +0600

    Add theme support with react-with-styles

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,17 @@
 import { PropTypes } from 'react';
+import Head from 'next/head';
 import Header from './Header';
+import stylesheet from '../static/index.css';
+import { css, withStyles } from '../lib/withStyles';
 
-const Layout = ({ children }) => (
-  <div>
+const Layout = ({ children, styles, title }) => (
+  <div {...css(styles.root)}>
+    <Head>
+      <title>{title}</title>
+      <meta charSet='utf-8' />
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+    </Head>
     <Header />
     {children}
   </div>
@@ -10,6 +19,19 @@ const Layout = ({ children }) => (
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
+  title: PropTypes.string,
 };
 
-export default Layout;
+Layout.defaultProps = {
+  title: 'This is a default title',
+};
+
+Layout.propTypes = {
+  styles: PropTypes.object.isRequired,
+};
+
+export default withStyles(({ color }) => ({
+  root: {
+    background: color.primary,
+  },
+}))(Layout);
--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,5 @@
 import Document, { Head, Main, NextScript } from 'next/document';
 import { StyleSheetServer } from 'aphrodite';
-import { fontFamilyBase } from '../lib/theme';
-import stylesheet from '../static/index.css';
 
 class MyDocument extends Document {
   static async getInitialProps({ renderPage }) {
@@ -10,19 +8,10 @@ class MyDocument extends Document {
   }
 
   render() {
-    const styles = [
-      stylesheet,
-      `html { font-family: ${fontFamilyBase};}`,
-      this.props.css.content
-    ];
-    
     return (
       <html lang="en">
         <Head>
-          <title>My page</title>
-          {styles.map(css =>
-            <style dangerouslySetInnerHTML={{ __html: css }} />,
-           )}
+          <style dangerouslySetInnerHTML={{ __html: this.props.css.content }} />
         </Head>
         <body>
           <Main />
