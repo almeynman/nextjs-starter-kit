776c682e1df1f610957982039e29bb430b88d5f2
Almas Akchabayev <almas.akchabayev@gmail.com>
2017-04-04 14:16:58 +0600

    Add special metas to head element

--- components/Layout.js
+++ components/Layout.js
@@ -6,13 +6,16 @@ import Feedback from './Feedback';
 import Footer from './Footer';
 import stylesheet from './Layout.css';
 
-const Layout = ({ children, styles, title }) => (
+const Layout = ({ children, styles, title, description }) => (
   <div {...css(styles.root)}>
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />{
-      // eslint-disable-next-line react/no-danger
+      <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+      <meta name="description" content={description} />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="apple-touch-icon" href="/static/apple-touch-icon.png" />{
+        // eslint-disable-next-line react/no-danger
       }<style dangerouslySetInnerHTML={{ __html: stylesheet }} />
     </Head>
     <Header />
@@ -26,10 +29,12 @@ Layout.propTypes = {
   children: PropTypes.node.isRequired,
   styles: PropTypes.object.isRequired,
   title: PropTypes.string,
+  description: PropTypes.string,
 };
 
 Layout.defaultProps = {
   title: 'This is a default title',
+  description: 'This is a default description',
 };
 
 export default withStyles(({ fontFamilyBase }) => ({
